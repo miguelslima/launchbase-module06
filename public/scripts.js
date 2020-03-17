c90cadcaefc716267d19c749b2b98f761982fab9
Miguel Lima <miguelsousalima@hotmail.com>
2020-03-17 10:59:26 -0300

    Starting upload of photos

--- public/scripts.js
+++ public/scripts.js
@@ -29,3 +29,51 @@ const Mask = {
     }).format(value/100);
   }
 }
+
+const PhotosUpload = {
+  preview: document.querySelector('#photos-preview'),
+  uploadLimit: 6,
+  handleFileInput(event)  {
+    const {files: fileList} = event.target;
+
+    if(PhotosUpload.hasLimit(event)) {
+      return
+    }
+
+    Array.from(fileList).forEach( file => {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        const image = new Image() // <img />   
+        image.src = String(reader.result);
+
+        const div = PhotosUpload.getContainer(image)
+
+        PhotosUpload.preview.appendChild(div)
+      }
+
+      reader.readAsDataURL(file);
+    })
+  },
+  hasLimit(event) {
+    const {uploadLimit} = PhotosUpload;
+
+    if(fileList.length > uploadLimit) {
+      alert(`Envie no máximo ${uploadLimit} fotos`)
+      event.preventDefault();
+      return true
+    }
+
+    return false;
+  },
+  getContainer(image) {
+    const div = document.createElement('div');
+    div.classList.add('photo');
+
+    div.onclick = () => alert('remover foto');
+
+    div.appendChild(image);
+
+    return div;
+  }
+}
--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,3 +1,5 @@
+const { formatPrice } = require('../../lib/utils');
+
 const Category = require('../models/Category');
 const Product = require('../models/Product');
 
@@ -35,9 +37,39 @@ module.exports = {
       return res.send("Product not found!");
     }
 
+    product.old_price = formatPrice(product.old_price);
+    product.price = formatPrice(product.price);
+
     results = await Category.all()
     const categories = results.rows;
 
     return res.render("products/edit.njk", { product, categories});
+  },
+  async put(req, res) {
+    const keys = Object.keys(req.body);
+
+    for(key of keys) {
+      if(req.body[key] == "") {
+        return res.send("Please, fill all fiedls");
+      }
+    }
+
+    req.body.price = req.body.price.replace(/\D/g, "")
+
+    if(req.body.old_price != req.body.price) {
+      const oldProduct = await Product.find(req.body.id);
+
+      req.body.old_price = oldProduct.rows[0].price;
+    }
+
+    await Product.update(req.body);
+
+    return res.redirect(`/products/${req.body.id}/edit`);
+  },
+  async delete(req, res) {
+
+    await Product.delete(req.body.id);
+
+    return res.redirect('/products/create');
   }
 }
