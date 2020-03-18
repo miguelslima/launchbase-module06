98f0a629271f17a5fa804fcbd4a32c80bc738683
Miguel Lima <miguelsousalima@hotmail.com>
2020-03-18 10:46:11 -0300

    Uploads photos finish

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -2,6 +2,7 @@ const { formatPrice } = require('../../lib/utils');
 
 const Category = require('../models/Category');
 const Product = require('../models/Product');
+const File = require('../models/File');
 
 module.exports = {
   create(req, res) {
@@ -23,9 +24,16 @@ module.exports = {
         return res.send("Please, fill all fiedls");
       }
     }
+
+    if(req.files.length == 0) {
+      return res.send("Please, send at least one image");
+    }
     
     let results = await Product.create(req.body)
     const productId = results.rows[0].id
+    
+    const filesPromise = req.files.map(file => File.create({...file, product_id: productId }));
+    await Promise.all(filesPromise);
 
     return res.redirect(`/products/${productId}`);
   }, 
